0365ced91fa3cab264a297f2570adc36af5231f6
laransom <laransom@gmail.com>
2014-03-19 15:56:04 -0400

    employees view set up and set up framework for customers view

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Korning::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :employees, only: [:index, :show]
+
 end
